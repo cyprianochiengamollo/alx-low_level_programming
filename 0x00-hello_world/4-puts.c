245b6a2634ba4c15ceebe667276f1b7d2bca90fd
cyprianochiengamollo <cyprianochy15@gmail.com>
2023-07-26 10:01:14 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
-#!/bin/bash
-init main(void)
+#include<stdio.h>
+int main(void)
+/**
+*main -main entry point
+return:always 0 (success)
+*/
+* int main (void);
 {
-	puts("\"Programming is like building a multilingual puzzle);
-	
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
